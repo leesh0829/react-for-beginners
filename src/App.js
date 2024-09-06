36851b68c9e4203b113a86ed961e3f8d18945bc4
Adelie <lsh77392125@gmail.com>
2024-09-06 17:07:10 +0900

    20240906-3

--- src/App.js
+++ src/App.js
@@ -1,58 +1,27 @@
-//import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("hi");
-    return () => console.log("bye");
-  }, []);
-
-  useEffect(() => {
-    console.log("hi");
-    return function () {
-      console.log("bye");
-    };
-  });
-
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  /*const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run every time");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    console.log("i run only once");
-  });
-  useEffect(() => {
-    console.log("SEARCH FOR", keyword);
-  }, [keyword]);
-  useEffect(() => {
-    console.log("run when counter changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter changes");
-  }, [keyword, counter]); */
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
-    /*<div>
-      <h1 className={styles.title}>Hello World</h1>
-      <input
-        value={keyword}
-        onChagne={onChange}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-      </div> */
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>The Coins ({coins.length})</h1>
+      {loading ? <strong>Loading..</strong> : null}
+      <ul>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/ToDoLists.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((cuttentArray) => [toDo, ...cuttentArray]); //0번 부터 배열에 밀어서 추가
+    setToDo("");
+  };
+  return (
+    <div>
+      <h1>My To Do Lists ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write ur to do"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </div>
+  );
+}
+
+export default App;
